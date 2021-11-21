f72564313b701fe85b49cc41cbd490f06e371f08
cmarioep <cmescandonpardo@gmail.com>
2021-11-21 16:48:14 -0500

    Generate box-shadow code on box__code

--- src/Components/Box.js
+++ src/Components/Box.js
@@ -9,10 +9,14 @@ function Box() {
     const spread = useSelector(state => state.box_spread);
     const color = useSelector(state => state.box_color);
 
+    const generatedCode = `box-shadow: ${xOffset}px ${yOffset}px ${blur}px ${spread}px rgba(${color.r},${color.g},${color.b},${color.a})`
+
+
     return(
         <div className="box" 
             style={{boxShadow: `${xOffset}px ${yOffset}px ${blur}px ${spread}px rgba(${color.r},${color.g},${color.b},${color.a})`}}>
-                <input type="text"/>
+            
+            <input className="box__code" type="text" value={generatedCode}/>
         </div>
     )
 };
