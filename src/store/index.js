416b442ae156ba5c6405842772fba858b2c83db3
cmarioep <cmescandonpardo@gmail.com>
2021-12-22 15:54:18 -0500

    Fix bug with items dimentions on desktop version

--- src/store/index.js
+++ src/store/index.js
@@ -17,7 +17,7 @@ const initialValues = {
 
     // Initial values for Glass-Morphism
     glassMorphism_blurFilter: 5,
-    glassMorphism_opacity: 0.075,
+    glassMorphism_opacity: 0.2,
     glassMorphism_border: 2,
     glassMorphism_borderRadius: 10,
     glassMorphism_color: {r: 22, g: 163, b: 211, a:1}
