160706edf0cb762dffe898058368076232513786
cmarioep <cmescandonpardo@gmail.com>
2022-07-22 12:24:58 -0500

    Fix: critical updates, move projecto to vite

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,12 @@ import App from './App';
 import {Provider} from 'react-redux';
 import store from './store/index';
 
-ReactDOM.render(
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
     <Provider store={store}>
         <App />
     </Provider>,
-    document.getElementById('root')
-);
+    </React.StrictMode>
+  );
