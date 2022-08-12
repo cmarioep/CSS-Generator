6de250a23f3c6bc0193e37f72d5b251ca9999225
cmarioep <cmescandonpardo@gmail.com>
2022-08-12 11:00:58 -0500

    Fix: layout container level up outside BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -7,17 +7,17 @@ import { BoxShadow, TextShadow, GlassMorphism } from './pages';
 
 function App() {
   return (
-    <div>
-      <BrowserRouter>
-          <Layout>
-            <Routes>
-              <Route path="/" element={<BoxShadow />} />
-              <Route path="/text-shadow" element={<TextShadow />} />
-              <Route path="/glass" element={<GlassMorphism />} />
-            </Routes>
-          </Layout>
-      </BrowserRouter>
-    </div>
+    <>
+      <Layout>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<BoxShadow />} />
+            <Route path="/text-shadow" element={<TextShadow />} />
+            <Route path="/glass" element={<GlassMorphism />} />
+          </Routes>
+        </BrowserRouter>
+      </Layout>
+    </>
   );
 }
 
