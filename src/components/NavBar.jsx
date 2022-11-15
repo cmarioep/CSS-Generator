c3fd5427186d242423650feb729c636474a79f52
cmarioep <cmescandonpardo@gmail.com>
2022-11-15 18:16:06 -0500

    Feature: navBar icons standar menu and close

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,6 @@
 import { NavLink } from "react-router-dom";
+import { CloseIcon } from "./icons/CloseIcon";
+import { MenuIcon } from "./icons/MenuIcon";
 
 
 import '../styles/components/NavBar.scss';
@@ -8,14 +10,14 @@ import '../styles/components/NavBar.scss';
 function NavBar() {
     return (
         <nav className='navBar'>
+            
+            <input className="navBar__button" id="menu__button" type="checkbox" />
 
             <label className="navBar__icon" htmlFor="menu__button">
-                <svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 50 50" width="50px" height="50px">
-                    <path d="M 0 7.5 L 0 12.5 L 50 12.5 L 50 7.5 Z M 0 22.5 L 0 27.5 L 50 27.5 L 50 22.5 Z M 0 37.5 L 0 42.5 L 50 42.5 L 50 37.5 Z"/>
-                </svg>
-            </label>
-                    
-            <input className="navBar__button" id="menu__button" type="checkbox" />
+                <MenuIcon className="navBar__icon--menu"/>
+                <CloseIcon className="navBar__icon--close"/>
+
+            </label>                    
 
             <ul className='navBar__menu'>
                 <NavLink className={({ isActive }) => `navBar__menu__item ${ isActive ? 'navBar__menu__item--isActive' : ''}`} to="/" >Box Shadow</NavLink>
---
+++ src/components/icons/MenuIcon.jsx
@@ -0,0 +1,11 @@
+export const MenuIcon = (props) => {
+
+    return (
+        <>
+            <svg viewBox="0 0 46 46" {...props}>
+                <path d="M0 7.5v5h50v-5Zm0 15v5h50v-5Zm0 15v5h50v-5Z" />
+            </svg>
+        </>
+    )
+
+}
